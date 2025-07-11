6218846d7c8015ecc8063fe49c1713d597f54bd0
btc-drakeman <wonny64@naver.com>
2025-07-11 16:21:39 +0900

    Add files via upload

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
 # ✅ 텔레그램 봇 설정
-BOT_TOKEN = '7213273799:AAEi6KtkzHqwKkrsQgaxSgKSnRWbXM70gUA'
-USER_IDS = ['7505401062', '7576776181']
+TELEGRAM_TOKEN = '7213273799:AAEi6KtkzHqwKkrsQgaxSgKSnRWbXM70gUA'
+TELEGRAM_CHAT_ID = ['7505401062', '7576776181']
 API_URL = f'https://api.telegram.org/bot{BOT_TOKEN}'
 
 # ✅ 분석 대상 심볼 (MEXC 선물 표기 기준)
