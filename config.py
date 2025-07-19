343ea7e48fecf5c9ee06381fd8e80076d56f34c2
btc-drakeman <wonny64@naver.com>
2025-07-19 22:30:42 +0900

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-
-# ✅ 텔레그램 봇 설정
-TELEGRAM_TOKEN = '7213273799:AAEi6KtkzHqwKkrsQgaxSgKSnRWbXM70gUA'
-TELEGRAM_CHAT_ID = ['7505401062', '7576776181']
-API_URL = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}'
-
-# ✅ 분석 대상 심볼 (MEXC 선물 표기 기준)
-SYMBOLS = ['BTCUSDT', 'ETHUSDT', 'ETHFIUSDT', 'SUIUSDT', 'XRPUSDT', 'DOGEUSDT']
